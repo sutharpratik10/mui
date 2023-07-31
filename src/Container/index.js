4591319d1da0e3bb4e51f17037421eeaa3b85023
Pratik <sutharpratik10@hotmail.com>
2023-08-01 01:23:21 +0530

    Adding Home page to dev repo commit

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,4 +1,5 @@
 export {default as Header} from './Header';
+export {default as Home} from './Home';
 export {default as Blog} from './Blog';
 export {default as Details} from './Details';
 export {default as Feature} from './Feature';
