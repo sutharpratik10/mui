d5fe8c41cc4337268b154380cd3237d263640138
Pratik <sutharpratik10@hotmail.com>
2023-08-03 12:45:48 +0530

    Implementing Home page

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import '../style/footer.css'
 function Footer() {
   return (
     
-<footer class="fixed bottom-0 left-0 z-20 w-full bg-white shadow">
+<footer class="bottom-0 left-0 z-20 w-full bg-white shadow dark:bg-slate-800">
     <div class="w-full mx-auto p-4 md:flex md:items-center md:justify-between">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">MUI™</a>. All Rights Reserved.
       </span>
--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -37,8 +37,8 @@ const products = [
    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
 <>
-    <header className="bg-slate-200 dark:bg-gray-800 z-10">
-        <nav className="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <header className="fixed top-0 bg-slate-200 dark:bg-gray-800 z-10 w-full">
+        <nav className="flex mx-auto items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <NavLink to='/' className="-m-1.5 p-1.5">
               <span className="sr-only">Multi UI</span>
@@ -46,13 +46,14 @@ const products = [
             </NavLink>
           </div>
           <div className="flex lg:hidden">
+          <Themeswitcher/>
             <button
               type="button"
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700 dark:text-white"
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              <Bars3Icon className="ml-5 h-6 w-6" aria-hidden="true" />
             </button>
           </div>
           <Popover.Group className="hidden lg:flex lg:gap-x-12">
@@ -134,6 +135,7 @@ const products = [
         </nav>
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
           <div className="fixed inset-0 z-10" />
+          
           <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-slate-800  px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5 ">
@@ -147,8 +149,7 @@ const products = [
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-white"
-                onClick={() => setMobileMenuOpen(false)}
-              >
+                onClick={() => setMobileMenuOpen(false)}>
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon className="h-6 w-6" aria-hidden="true" />
               </button>
