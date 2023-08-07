d99e1ffbde9e96692c9502e2499b05ed16748962
Pratik Suthar <sutharpratik10@hotmail.com>
2023-08-08 02:26:58 +0530

    Removed Accordion

--- src/Components/shortcodes/Accordions.jsx
+++ src/Components/shortcodes/Accordions.jsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
+import {useState, useEffect} from 'react';
 
 import {accordionItems, accordionoptions} from '../data'
 
-function BasicAccordion() {
-    
+function BasicAccordion () {
+  
   return (
       <div className="text-center">
         <h2 className="text-[48px] my-10 font-bold leading-7 text-indigo-600 dark:text-indigo-400">Accordions</h2>
         <hr className='mb-10'/>
         <div className="mx-auto text-left">
-          <p className="text-[24px] font-semibold  tracking-tight text-gray-900 dark:text-white">Simple Accordions</p>
+          <p className="text-[24px] font-semibold  tracking-tight text-gray-900 dark:text-white">Accordions Gray</p>
         </div>
         <div className="mb-10 py-3 ">
-
+          
         </div>
         <hr className='mb-10'/>
         
