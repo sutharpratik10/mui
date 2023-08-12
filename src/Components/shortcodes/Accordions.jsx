7a05d8f1dbfe80777e4c4fa9d1e582ebc146a71a
Pratik <sutharpratik10@hotmail.com>
2023-08-12 15:03:35 +0530

    implemented accordion more dynamic

--- src/Components/shortcodes/Accordions.jsx
+++ src/Components/shortcodes/Accordions.jsx
@@ -4,44 +4,38 @@ import Header from './Headers'
 import {accordionItems} from '../data'
 
 function AccordionItem({
-  item: { title, description }, // Use "item" prop here
   active,
   onClickItem,
+  children,
+  className
 }) {
- return (
-  <li
-      className={`transition ease-in-out ${
-        active ? 'border-0' : 'border-0'
-      }`}
-    >
+  return (
+    <li className={`transition ease-in-out ${active ? 'border-0' : 'border-0'}`}>
       <div className='bg-gray-200 dark:bg-slate-800 mb-2'>
         <button
-          className={`w-full font-bold text-left p-4 outline-none cursor-pointer relative`}
-          onClick={onClickItem}
-        >
-          {title}
+          className={`w-full font-bold text-left p-4 outline-none cursor-pointer relative  ${className}`}
+          onClick={onClickItem}>
+          {children.title}
           <span
-            className={`absolute top-1/4 right-5 transform translate-x-1/2 transition ease-in-out  ${
-              active ? 'rotate-180' : ''
-            }`}
-          >
+            className={`absolute top-1/4 right-5 transform translate-x-1/2 transition ease-in-out ${className} ${
+              active ? 'rotate-180' : ''}`}>
             &#x2304;
           </span>
         </button>
       </div>
       
       <div
-        className={`transition-all ${
+        className={`transition-all  ${className} ${
           active ? 'max-h-100 mb-10' : 'max-h-0 opacity-0'
         } overflow-y-scroll`}
       >
-        {description}
+        {children.description}
       </div>
     </li>
- )
+  );
 }
 
-function Accordion({type}) {
+function Accordion({type, children, className}) {
   const [openIndexes, setOpenIndexes] = React.useState([0])
   
   const handleAccordion = (idx) => {
@@ -60,15 +54,13 @@ function Accordion({type}) {
   }
   
   return(
-    <div className="">
-      <ul className=''>
-        {accordionItems.map((accordionItem, idx) => (
-          <AccordionItem 
-             key={accordionItem.id} 
-             item={accordionItem}
-             onClickItem={() => handleAccordion(idx)}
-             active={openIndexes.includes(idx)}
-             itemId={accordionItem.id} />
+    <div className={className}>
+      <ul>
+        {React.Children.map(children, (child, idx) => (
+          React.cloneElement(child, {
+            onClickItem: () => handleAccordion(idx),
+            active: openIndexes.includes(idx),
+          })
         ))}
       </ul>
     </div>
@@ -77,25 +69,35 @@ function Accordion({type}) {
 
 function BasicAccordion () {
   return (
-      <div className="">
-        
+      <div>
         <Header>Accordions</Header>
         <hr className='mb-10'/>
         <div className="mx-auto text-left">
           <p className="text-[24px] font-semibold  tracking-tight text-gray-900 dark:text-white">Accordion Standard</p>
         </div>
         <div className="mb-10 py-3 ">
-          <Accordion type="Standard"/>
+          <Accordion type="Standard" className="your-custom-class">
+            {accordionItems.map((item, idx) => (
+              <AccordionItem key={idx} className="your-custom-class">
+                {item}
+              </AccordionItem>
+            ))}
+          </Accordion>
         </div>
         <hr className='mb-10'/>
         <div className="mx-auto text-left">
           <p className="text-[24px] font-semibold  tracking-tight text-gray-900 dark:text-white">Accordion Single</p>
         </div>
         <div className="mb-10 py-3 ">
-          <Accordion type="Single"/>
+          <Accordion type='Single' className="your-custom-class">
+            {accordionItems.map((item, idx) => (
+              <AccordionItem key={idx} className="your-custom-class">
+                {item}
+              </AccordionItem>
+            ))}
+          </Accordion>
         </div>
         <hr className='mb-10'/>
-        
       </div>
   );
 }
